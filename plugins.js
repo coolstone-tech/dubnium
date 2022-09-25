75d12b78a15fab2ae86e3f00ada17c5b3a3ce47d
System <system@coolstone.dev>
2022-09-25 11:13:32 -0700

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -2,5 +2,4 @@
 module.exports = {
     package_example: require("dubnium-test-pkg"),
     function_example() { console.log("Hello, world") },
-    file_example: require("fs").readFileSync("./cool_plugin.js")
 }
